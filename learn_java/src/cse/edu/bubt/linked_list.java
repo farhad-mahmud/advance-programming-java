9b80488f5fc8f5792b65ad469c27e3d250ddf5a1
farhad-mahmud <rixonahmed@gmail.com>
2025-07-02 04:15:26 +0600

    end

--- learn_java/src/cse/edu/bubt/linked_list.java
+++ learn_java/src/cse/edu/bubt/linked_list.java
@@ -1,6 +1,26 @@
 package cse.edu.bubt;
+import java.util.Scanner ;
+import java.util.LinkedList;
+import java.util.Collections ;
 
 public class linked_list {
 
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in) ;
 
+         LinkedList<Integer> numbers = new LinkedList<>() ;
+
+         int n = sc.nextInt() ;
+
+         for(int i =0 ;i<n;i++){
+               int num = sc.nextInt() ;
+               numbers.add(num) ;
+
+         }
+
+         int min_val = Collections.min(numbers) ;
+
+         System.out.println("min value" + min_val ;
+
+     }
 }
